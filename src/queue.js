55208b1dfac0eeaeb8e5ea52200ad3323651f2f1
solowihmaria <solowihmaria@gmail.com>
2024-12-26 15:43:40 +0300

    feat: implement Queue using linked list with enqueue, dequeue, and getUnderlyingList methods

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.items = []; // инициализируем пустой массив для хранения элементов очереди
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // метод для добавления  в конец очереди
+  enqueue(value) {
+    this.items.push(value); // добавляем элемент в конец массива
   }
 
+  // метод для удаления  из начала очереди
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.items.length === 0) {
+      // если очередь пуста
+      return null;
+    }
+    return this.items.shift(); // удаляем и возвращаем первый элемент массива
+  }
+
+  // метод для получения текущего состояния очереди
+  getUnderlyingList() {
+    // преобразуем массив this.items в связанный список с конца к началу
+    const list = this.items.reduceRight((next, value) => {
+      // на каждой итерации создаем новый узел связанного списка ( содержит текущ и ссылку на след)
+      return { value, next };
+    },null);//начальное значение (next) последнего эл т.к. он не ссылается
+
+    return list;
   }
 }
 
